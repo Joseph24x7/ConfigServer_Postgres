097d9dd7d462827eb399c614114c725ea08a0a85
joseph24x7 <joseph2471994@gmail.com>
2023-02-18 22:40:06 +0530

    docker revert

--- src/main/java/com/config/server/ConfigServerApplication.java
+++ src/main/java/com/config/server/ConfigServerApplication.java
@@ -2,7 +2,6 @@ package com.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
